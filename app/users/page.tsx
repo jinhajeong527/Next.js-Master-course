85672e4209b9d370afd6ff40179a84310b33368c
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-10-21 13:25:30 +0900

    feat: server-side user data fetch

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const UsersPage = () => {
+interface User {
+    id: number;
+    name: string;
+}
+
+const UsersPage = async () => {
+    const res =  await fetch('https://jsonplaceholder.typicode.com/users')
+    const users: User[] = await res.json();
+    // no state and effect hook here
+    // our bundle is going to be smaller because this component will be on the server.
     return (
-        <div>
-            UsersPage
-        </div>
+        <>
+            <h1>Users</h1>
+            <ul>
+                {users.map(user => <li key={user.id}>{user.name}</li>)}
+            </ul>
+        </>
     );
 };
 
