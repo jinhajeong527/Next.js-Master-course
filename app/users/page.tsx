838a75819874b579cd5309e1b07ec01f60e56ce2
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-10-22 16:46:34 +0900

    refactor: create UserTable component in app/user directory for local use

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,42 +1,14 @@
 import React from 'react';
-
-interface User {
-    id: number;
-    name: string;
-    email: string;
-}
+import UserTable from "@/app/users/UserTable";
 
 const UsersPage = async () => {
-    const res = await fetch(
-        // Next.js is gonna run back ground job and get fresh
-        // data from the backend every 10 seconds.
-        'https://jsonplaceholder.typicode.com/users',
-        {cache: 'no-store'});
-    // {next: {revalidate: 10}});
-    const users: User[] = await res.json();
+
     // no state and effect hook here
     // our bundle is going to be smaller because this component will be on the server.
     return (
         <>
             <h1>Users</h1>
-            {/*To see when this page is rendered*/}
-            <p>{new Date().toLocaleTimeString()}</p>
-            <table className='table table-bordered'>
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                </tr>
-
-                </thead>
-                <tbody>
-                {users.map(user =>
-                    <tr key={user.id}>
-                        <td>{user.name}</td>
-                        <td>{user.email}</td>
-                    </tr>)}
-                </tbody>
-            </table>
+            <UserTable />
         </>
     );
 };
