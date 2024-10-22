e86075957c6041e918c85e592777d8f5f0e000d3
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-10-22 15:52:04 +0900

    feat: daisy ui

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 interface User {
     id: number;
     name: string;
+    email: string;
 }
 
 const UsersPage = async () => {
@@ -10,16 +11,32 @@ const UsersPage = async () => {
         // Next.js is gonna run back ground job and get fresh
         // data from the backend every 10 seconds.
         'https://jsonplaceholder.typicode.com/users',
-        {next: {revalidate: 10}});
+        {cache: 'no-store'});
+    // {next: {revalidate: 10}});
     const users: User[] = await res.json();
     // no state and effect hook here
     // our bundle is going to be smaller because this component will be on the server.
     return (
         <>
             <h1>Users</h1>
-            <ul>
-                {users.map(user => <li key={user.id}>{user.name}</li>)}
-            </ul>
+            {/*To see when this page is rendered*/}
+            <p>{new Date().toLocaleTimeString()}</p>
+            <table className='table table-bordered'>
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+
+                </thead>
+                <tbody>
+                {users.map(user =>
+                    <tr key={user.id}>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                    </tr>)}
+                </tbody>
+            </table>
         </>
     );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,25 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
+import daisyui from 'daisyui';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [
+        daisyui,
+    ],
+    daisyui: {
+        themes: ["winter"],
+    },
 };
 export default config;
