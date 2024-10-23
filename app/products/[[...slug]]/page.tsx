ad2cfbf4651c7ccd3a569e73cb1c8ba5a9ebdd38
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-10-23 11:01:18 +0900

    feat: adduser table sort function

--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 
 interface Props {
     params: { slug: string[] }
+    searchParams: { sortOrder: string }
 }
+
 // We should pass at least one parameter
 // To make this slug parameter optional,
 // directory should be wrapped with double square bracket
-const ProductPage = ({params: {slug}}: Props) =>
-{
-    return (
-        <div>
-            {slug}
-        </div>
-    );
-}
+const ProductPage = ({params: {slug}, searchParams: {sortOrder}}: Props) => {
+        return (
+            <div>
+                {slug}
+                {sortOrder}
+            </div>
+        );
+    }
 ;
 
 export default ProductPage;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import UserTable from "@/app/users/UserTable";
 
-const UsersPage = async () => {
+interface Props {
+    searchParams: { sortOrder: string }
 
+}
+
+const UsersPage = async ({searchParams: {sortOrder}}: Props) => {
     // no state and effect hook here
     // our bundle is going to be smaller because this component will be on the server.
+    // Whenever we need to access to the query string parameters or URL
+    // We have to do that in our pages
     return (
         <>
             <h1>Users</h1>
-            <UserTable />
+            <UserTable sortOrder={sortOrder} />
         </>
     );
 };
