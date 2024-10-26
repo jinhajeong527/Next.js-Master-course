9d61fd13a7bef4396a6d2fbe2e3c41e9a1769096
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-10-27 07:43:50 +0900

    feat: add function to view uploaded images

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,14 +1,29 @@
 'use client';
-import {CldUploadWidget} from "next-cloudinary";
+import {CldUploadWidget, CldImage} from "next-cloudinary";
+import {useState} from "react";
+
+interface CloudinaryResult {
+    public_id: string;
+}
 
 const UploadPage = () => {
+    const [publicId, setPublicId] = useState('');
     return (
-        <CldUploadWidget uploadPreset='ujtmnzkc'>
-            {({open}) => <button
-                className='btn btn-primary'
-                onClick={() => open()}>Upload</button>
-            }
-        </CldUploadWidget>
+        <>
+            {publicId && <CldImage alt='A Image' src={publicId} width={270} height={180}/>}
+            <CldUploadWidget
+                uploadPreset='ujtmnzkc'
+                onSuccess={(results, widget) => {
+                    if (results.event !== 'success') return;
+                    const info = results.info as CloudinaryResult;
+                    setPublicId(info.public_id);
+                }}>
+                {({open}) => <button
+                    className='btn btn-primary'
+                    onClick={() => open()}>Upload</button>
+                }
+            </CldUploadWidget>
+        </>
     );
 };
 
