56bcb99226bbc2e1e9113b225ddbd8e18099f5d1
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-10-31 11:11:40 +0900

    feat: configure CredentialsProvider for username signin

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,33 @@
 import NextAuth, {NextAuthOptions} from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import {PrismaAdapter} from "@auth/prisma-adapter";
 import prisma from "@/prisma/client"
+import bcrypt from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
-    providers:[
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: {label: 'Email', type: 'email', placeholder: 'Email'},
+                password: {label: 'Password', type: 'password', placeholder: 'Password'}
+            },
+            async authorize(credentials, req) {
+                if (!credentials?.email || !credentials.password) {
+                    return null;
+                }
+                const user = await prisma.user.findUnique({
+                    where: {email: credentials.email}
+                })
+                if (!user) return null;
+                const passwordsMatch = await bcrypt.compare(
+                    credentials.password, user.hashedPassword!
+                );
+                return passwordsMatch ? user : null;
+            },
+        }),
         GoogleProvider({
             // We know that we have set this up properly
             // So we can put an exclamation mark at the end
@@ -16,7 +38,6 @@ export const authOptions: NextAuthOptions = {
         })
     ],
     pages: {
-        signIn: '/dashboard'
     }
     // As the time of the video recorded, this option was needed
     // to store JWT session info to the Database
