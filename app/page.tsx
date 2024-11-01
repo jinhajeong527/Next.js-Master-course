2915f3d6b1c8788de0d4ae41cc6929421b321e2a
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-11-01 11:32:47 +0900

    feat: add local image using Next.js Image component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,29 @@
-import Link from "next/link";
-import ProductCard from "@/app/components/ProductCard/ProductCard";
-import {getServerSession} from "next-auth";
-import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+// import Link from "next/link";
+// import ProductCard from "@/app/components/ProductCard/ProductCard";
+// import {getServerSession} from "next-auth";
+// import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import Image from "next/image";
+import croissant from "@/public/images/croissant.png";
+
+// export default async function Home() {
+//     // We can use this in our pages as well as our route handlers
+//     const session = await getServerSession(authOptions);
+//     return (
+//         <main>
+//             <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+//             <Link href={"/users"}>Users</Link>
+//             <ProductCard/>
+//         </main>
+//     );
+// }
 
 export default async function Home() {
-    // We can use this in our pages as well as our route handlers
-    const session = await getServerSession(authOptions);
     return (
         <main>
-            <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-            <Link href={"/users"}>Users</Link>
-            <ProductCard/>
+            <Image
+                src={croissant}
+                alt="Croissant"
+            />
         </main>
-    );
+    )
 }
