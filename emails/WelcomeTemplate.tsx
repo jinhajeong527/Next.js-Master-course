dc54576b2f65b4b48e2734e4cd3fa453bbdb4a43
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-11-01 10:09:54 +0900

    style: add Tailwind and inline styles to WelcomeTemplate

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import {Html, Body, Container, Text, Link, Preview} from '@react-email/components';
+import React, {CSSProperties} from 'react';
+import {
+    Html,
+    Body,
+    Container,
+    Tailwind,
+    Text,
+    Link,
+    Preview
+} from '@react-email/components';
 
 const WelcomeTemplate = ({name}: { name: string }) => {
     return (
         <Html>
             <Preview>Welcome aboard!</Preview>
-            <Body>
-                <Container>
-                    <Text>Hello {name}</Text>
-                    <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
-                </Container>
-            </Body>
+            <Tailwind>
+                <Body className={"bg-pink-300"}>
+                    <Container>
+                        <Text style={heading}>Hello {name}</Text>
+                        <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
+                    </Container>
+                </Body>
+            </Tailwind>
         </Html>
     );
 };
+
+const heading: CSSProperties = {
+    fontSize: '32px',
+}
 
 export default WelcomeTemplate;
